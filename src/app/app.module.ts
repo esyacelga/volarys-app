4000cf9c3578941170b280aa0b51cc391416c451
Santiago Yacelga <esyacelga@hotmail.com>
2018-10-02 20:28:43 -0500

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,17 @@ import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModule} from './material';
 import {LayoutModule} from '@angular/cdk/layout';
 import {MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule} from '@angular/material';
+
+// Rutas
+import {APP_ROUTING} from './app.routes';
+
+// Componentes
 import {HeaderComponent} from './components/type-scripts/header.component';
 import {BodyComponent} from './components/type-scripts/body.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { HomeComponent } from './components/home/home.component';
+import {FooterComponent} from './components/footer/footer.component';
+import {HomeComponent} from './components/home/home.component';
+import {AbautComponent} from './components/abaut/abaut.component';
+import {HeroesComponent} from './components/heroes/heroes.component';
 
 
 @NgModule({
@@ -21,9 +28,12 @@ import { HomeComponent } from './components/home/home.component';
     HeaderComponent,
     BodyComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    AbautComponent,
+    HeroesComponent
   ],
   imports: [
+    APP_ROUTING,
     BrowserModule,
     BrowserAnimationsModule,
     NoopAnimationsModule,
