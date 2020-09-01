b263204de01090a5e43b3736d5300557bf224f98
santiago.yacelga <esyacelga@hotmail.com>
2020-08-31 19:59:26 -0500

    Cambio de estilo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   url: 'http://164.90.231.150:3000'
 };
 
